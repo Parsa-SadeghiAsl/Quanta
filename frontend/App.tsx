7d0964a82efcca05bd9694807f814f172f44a519
Parsa Sadeghi Asl <parsasadeghiasl@gmail.com>
2025-09-12 11:52:51 +0330

    ui: implemented SafeAreaView

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,21 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './state/queryClient';
 import { AuthProvider } from './hooks/useAuth';
 import AppNavigator from './navigation/AppNavigator';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    // This QueryClientProvider is for data fetching
-    <QueryClientProvider client={queryClient}>
-      {/* This PaperProvider is required for react-native-paper components */}
-      <PaperProvider>
-        {/* This AuthProvider manages user login state */}
-        <AuthProvider>
-          <AppNavigator />
-        </AuthProvider>
-      </PaperProvider>
-    </QueryClientProvider>
+    <SafeAreaView style={{ flex: 1}}>
+      <QueryClientProvider client={queryClient}>
+        <PaperProvider>
+          <AuthProvider>
+            <AppNavigator />
+          </AuthProvider>
+        </PaperProvider>
+      </QueryClientProvider>
+    </SafeAreaView>
+
   );
 }
